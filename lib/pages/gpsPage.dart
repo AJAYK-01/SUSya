9033c7f31b20008eab51986bab40e6a8c5d62de8
AntonySJohn <antonysjohn@cet.ac.in>
2021-05-29 20:56:52 +0530

    GPS function

---
+++ lib/pages/gpsPage.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:susya/gps/gpsUtil.dart';
+
+class GPSPage extends StatelessWidget {
+  const GPSPage({Key ?key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Location Sharing"),
+        backgroundColor: Colors.green,
+      ),
+      body: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  padding: EdgeInsets.only(bottom: 20),
+                  child: Text(
+                    "Dummy text about gps sharing",
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                        onPressed: (){
+                          determinePosition();
+                          //Go to next page
+                        },
+                        child: Container(
+                            child: Text("Agree"),
+                          padding: EdgeInsets.all(10),
+                        ),
+                    ),
+                    Container(
+                      width: 40,
+                    ),
+                    ElevatedButton(
+                      onPressed: (){
+                        //Go to next page
+                      },
+                      child: Text("Disagree"),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
